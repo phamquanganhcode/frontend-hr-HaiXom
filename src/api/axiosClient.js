a00a911591bda318ea8661d40961e2bf237f8f85
phamquanganhcode <phamanhh25@gmail.com>
2026-03-06 16:04:41 +0700

    chore: tạm lưu thay đổi ở axiosClient trước khi pull code

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   // Sử dụng biến môi trường từ Vite
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api', 
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api/v1', 
   headers: {
     'Content-Type': 'application/json',
   },
